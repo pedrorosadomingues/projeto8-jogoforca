e0871b11fe49de71c96171351a9acc13487e5622
pedrorosadomingues@gmail.com <pedrorosadomingues@gmail.com>
2022-11-24 13:09:50 -0300

    finalizado sem bonus

--- src/components/Jogo.js
+++ src/components/Jogo.js
@@ -7,11 +7,11 @@ export default function Jogo({ palavraState, jogoState, setJogoState, forcaState
             <img src={forcaState.imagem} alt="forca" />
             <button onClick={iniciarJogo}>Escolher Palavra</button>
             <div id="palavra" className={forcaState.contadorErros === 6 ? "vermelho" : jogoState.contadorAcertos === palavraState.length ? "verde" : null}>
-                {palavraState.map((l, index) => l ? <span
+                {palavraState.map((l, index) => l.letra  ? <span
                     key={index}
                     className={jogoState.status ? null : "hidden"}>
                     {l.acertou || forcaState.contadorErros === 6 || jogoState.contadorAcertos === palavraState.length ? ' ' + l.letra : ` _`}
-                </span> : null)}
+                </span> : "")}
             </div>
         </div>
     )
