f141ed9b79be5a9b3b383f194ab663046a1fec85
pedrorosadomingues@gmail.com <pedrorosadomingues@gmail.com>
2022-11-27 19:10:06 -0300

    quase tudo pronto

--- src/components/App.js
+++ src/components/App.js
@@ -10,20 +10,22 @@ const arrayPalavraObj = []; //array que recebera os objetos com as letras da pal
 arrayLetras.forEach((i) => arrayPalavraObj.push({ letra: i, acertou: false , contadorAcertos:0})); //cria um objeto { letra: , acerto: false} para cada letra da palavra sorteada e adiciona ao array de objetos.
 
 export default function App() {
-  console.log(palavra)
   const [palavraState, setPalavraState] = useState(arrayPalavraObj);// estado com a palavra em forma de array com objetos { letra: , acertou: } para componente jogo usar como condição e componente letras manipular
   const [jogoState, setJogoState] = useState({status:false , contadorAcertos:0});// estado que controla inicio do jogo manipulado pelo botao do componente jogo
   const [forcaState, setForcaState] = useState({imagem:'assets/forca.svg' , contadorErros:0});// estado que controla o numero de erros do jogador
   const [chuteState, setChuteState] = useState('');// estado que controla o chute do jogador
-  
+  console.log(palavra)
   return (
     <div className="App">
       <Jogo 
       palavraState={palavraState} //palavra sorteada em forma de array com objetos { letra: , acertou:  } para o componente Jogo usar como condicao na renderizacao da palavra
       jogoState={jogoState} //  estado controlador do inicio do jogo  manipulado pelo botao do componente Jogo
       setJogoState={setJogoState} // funcao que altera estado controlador do inicio do jogo
+      setForcaState={setForcaState} // funcao que altera estado controlador do numero de erros do jogador
+      setChuteState={setChuteState} // funcao que altera estado controlador do chute do jogador
       forcaState={forcaState} // estado que altera SRC da imagem da forca
       palavra={palavra} // palavra sorteada
+      setPalavraState={setPalavraState} // funcao que altera estado controlador da palavra sorteada
       />
       <Letras
         arrayLetras={arrayLetras} // array com as letras da palavra selecionada para componente encontrar indices de letras repetidas
--- src/components/Jogo.js
+++ src/components/Jogo.js
@@ -1,6 +1,17 @@
-export default function Jogo({ palavraState, jogoState, setJogoState, forcaState, palavra }) {
+import palavra from '../utils/palavras'; // importa palavra ja sorteada
+const arrayLetras = palavra.split(''); //transforma a palavra sorteada em um array de strings para ser manipulado com metodos de array
+const arrayPalavraObj = []; //array que recebera os objetos com as letras da palavra sorteada e o estado de acerto  { letra: , acerto: false }
+arrayLetras.forEach((i) => arrayPalavraObj.push({ letra: i, acertou: false , contadorAcertos:0})); //cria um objeto { letra: , acerto: false} para cada letra da palavra sorteada e adiciona ao array de objetos.
+
+export default function Jogo({ palavraState, jogoState, setJogoState,setForcaState, setChuteState,setPalavraState, forcaState, palavra }) {
     function iniciarJogo() {
-        setJogoState({...jogoState , status:true});
+        if ( jogoState.contadorAcertos === palavra.length ) {
+            setJogoState({status:false , contadorAcertos:0});
+            setForcaState({imagem:'assets/forca.svg' , contadorErros:0});
+            setChuteState('');
+            setPalavraState(arrayPalavraObj);
+        }
+        setJogoState({status:true , contadorAcertos:0});
     }
     return (
         <div id="jogo">
